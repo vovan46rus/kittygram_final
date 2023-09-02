cf627dde1e01c8167d463ce047a78d70476f5524
vovan46rus <punk.potap@gmail.com>
2023-09-02 10:52:53 +0300

    s_key

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('S_KEY')
 
 DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() == 'True'
 
