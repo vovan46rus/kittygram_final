b9dea46091b2a571479e8bb5c96f783a25e1ffd8
vovan46rus <punk.potap@gmail.com>
2023-09-02 17:29:54 +0300

    pytest settings env

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,7 +3,9 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env') 
+
+load_dotenv(dotenv_path)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
